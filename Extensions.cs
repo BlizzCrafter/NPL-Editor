3c6a67186c88b2aba776daadc42d9e897c40ec67
BlizzCrafter <m.haertel@gmx.net>
2024-04-19 17:54:16 +0200

    Added color string to Num.Vector4 extension.

--- Extensions.cs
+++ Extensions.cs
@@ -18,6 +18,15 @@ namespace NPLTOOL
                 (byte)(Microsoft.Xna.Framework.MathHelper.Clamp(color.W, 0f, 1f) * 255f)
             );
         }
+
+        public static Vector4 Parse(this string colorString)
+        {
+            string[] rgbValues = colorString.Split(',');
+            int red = int.Parse(rgbValues[0]);
+            int green = int.Parse(rgbValues[1]);
+            int blue = int.Parse(rgbValues[2]);
+            int alpha = int.Parse(rgbValues[3]);
+            return new Vector4(red / 255f, green / 255f, blue / 255f, alpha / 255);
         }
     }
 }
