0002eff2e479c84d75b273eb4f1753d7879f016f
BlizzCrafter <m.haertel@gmx.net>
2024-04-24 16:48:37 +0200

    Delete unused stuff.

--- NPLTOOL/Main.cs
+++ NPLTOOL/Main.cs
@@ -5,7 +5,6 @@ using NPLTOOL.Common;
 using NPLTOOL.GUI;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
@@ -20,7 +19,6 @@ namespace NPLTOOL
         private GraphicsDeviceManager _graphics;
         private ImGuiRenderer _imGuiRenderer;
 
-        private Texture2D _xnaTexture;
         private Num.Vector3 clear_color = new Num.Vector3(114f / 255f, 144f / 255f, 154f / 255f);
 
         private JsonNode _jsonObject;
@@ -58,15 +56,6 @@ namespace NPLTOOL
 
         protected override void LoadContent()
         {
-            // Texture loading example
-
-            // First, load the texture as a Texture2D (can also be done using the XNA/FNA content pipeline)
-            _xnaTexture = CreateTexture(GraphicsDevice, 300, 150, pixel =>
-            {
-                var red = (pixel % 300) / 2;
-                return new Color(red, 1, 1);
-            });
-
             //TODO: Use filepath of a startup argument instead.
             //string[] args = Environment.GetCommandLineArgs();
 
@@ -84,13 +73,8 @@ namespace NPLTOOL
         {
             GraphicsDevice.Clear(new Color(clear_color.X, clear_color.Y, clear_color.Z));
 
-            // Call BeforeLayout first to set things up
             _imGuiRenderer.BeforeLayout(gameTime);
-
-            // Draw our UI
             ImGuiLayout();
-
-            // Call AfterLayout now to finish up and draw all the things
             _imGuiRenderer.AfterLayout();
 
             base.Draw(gameTime);
@@ -383,25 +367,6 @@ namespace NPLTOOL
             }
             value = string.Empty;
             return false;
-        }
-
-        public static Texture2D CreateTexture(GraphicsDevice device, int width, int height, Func<int, Color> paint)
-        {
-            //initialize a texture
-            var texture = new Texture2D(device, width, height);
-
-            //the array holds the color for each pixel in the texture
-            Color[] data = new Color[width * height];
-            for (var pixel = 0; pixel < data.Length; pixel++)
-            {
-                //the function applies the color according to the specified pixel
-                data[pixel] = paint(pixel);
-            }
-
-            //set the color
-            texture.SetData(data);
-
-            return texture;
         }
     }
 }
