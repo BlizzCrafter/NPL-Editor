e4bf737b00af887f96130a2d68ef0be9f16cd5f8
BlizzCrafter <m.haertel@gmx.net>
2024-05-07 22:28:19 +0200

    Better description.

--- NPL-Editor/Main.cs
+++ NPL-Editor/Main.cs
@@ -165,7 +165,7 @@ namespace NPLEditor
                         {
                             AddContentDescriptor.Name = data.Key;
                             AddContentDescriptor.Category = data.Key;
-                            ModalDescriptor.Set(MessageType.EditContent, "Define name of your content.");
+                            ModalDescriptor.Set(MessageType.EditContent, "Set the name of your new content.");
                         }
 
                         var importerName = data.Value["importer"]?.ToString();
