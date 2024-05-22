2436ff6586d7681c66e794110f3555f13f4c34e2
BlizzCrafter <m.haertel@gmx.net>
2024-05-22 16:25:37 +0200

    Set readonly strings.

--- NPLEditor/AppSettings.cs
+++ NPLEditor/AppSettings.cs
@@ -6,9 +6,11 @@ namespace NPLEditor
     public static class AppSettings
     {
         public static readonly string GitHubRepoURL = "https://github.com/BlizzCrafter/NPL-Editor";
-        public static string LogsPath = Path.Combine(AppContext.BaseDirectory, "logs");
-        public static string AllLogPath = Path.Combine(LogsPath, "log.txt");
-        public static string ImportantLogPath = Path.Combine(LogsPath, "important-log.txt");
-        public static string BuildContentLogPath = Path.Combine(LogsPath, "build.txt");
+
+        public static readonly string LocalContentPath = Path.Combine(AppContext.BaseDirectory, "Content");
+        public static readonly string LogsPath = Path.Combine(AppContext.BaseDirectory, "logs");
+        public static readonly string AllLogPath = Path.Combine(LogsPath, "log.txt");
+        public static readonly string ImportantLogPath = Path.Combine(LogsPath, "important-log.txt");
+        public static readonly string BuildContentLogPath = Path.Combine(LogsPath, "build.txt");
     }
 }
