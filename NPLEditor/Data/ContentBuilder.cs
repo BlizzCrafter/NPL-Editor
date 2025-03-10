dd8c94374bda9f3b2ea81d00acc4e5941ca12d85
BlizzCrafter <7989230+BlizzCrafter@users.noreply.github.com>
2025-03-10 15:06:39 +0100

    Add: ContentBuilder class and using it to enable content building on app start (without loading the editor window).

---
+++ NPLEditor/Data/ContentBuilder.cs
@@ -0,0 +1,235 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.RuntimeBuilder;
+using NPLEditor.Common;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.Json.Nodes;
+using System.Threading;
+using System.Threading.Tasks;
+using ContentItem = NPLEditor.Common.ContentItem;
+
+namespace NPLEditor.Data
+{
+    public static class ContentBuilder
+    {
+        public static Dictionary<string, ContentItem> ContentList = new();
+        public static RuntimeBuilder RuntimeBuilder { get; set; }
+        public static string OutputPath => Path.Combine(Path.GetFullPath(OutputDir), TargetPlatform.ToString());
+        public static string IntermediatePath => Path.Combine(Path.GetFullPath(IntermediateDir), TargetPlatform.ToString());
+        public static string OutputDir = "bin";
+        public static string IntermediateDir = "obj";
+        public static string ContentRoot = "";
+        public static TargetPlatform TargetPlatform = TargetPlatform.DesktopGL;
+        public static GraphicsProfile GraphicsProfile = GraphicsProfile.Reach;
+        public static bool Compress = false;
+        public static bool CancelBuildContent = false;
+        public static bool BuildContentRunning = false;
+
+        public static void Init(JsonNode jsonObject)
+        {
+            ContentRoot = jsonObject["root"].ToString();
+            IntermediateDir = jsonObject["intermediateDir"].ToString();
+            OutputDir = jsonObject["outputDir"].ToString();
+            TargetPlatform = Enum.Parse<TargetPlatform>(jsonObject["platform"].ToString());
+            GraphicsProfile = Enum.Parse<GraphicsProfile>(jsonObject["graphicsProfile"].ToString());
+            Compress = bool.Parse(jsonObject["compress"].ToString());
+
+            RuntimeBuilder = new RuntimeBuilder(
+                Directory.GetCurrentDirectory(),
+                IntermediatePath,
+                OutputPath,
+                TargetPlatform,
+                GraphicsProfile,
+                Compress)
+            {
+                Logger = new NPLBuildLogger()
+            };
+
+            var references = GetAllReferences(jsonObject["references"].AsArray());
+            PipelineTypes.Load(references);
+            RuntimeBuilder.AddReferences(references);
+
+            ParseAllContentFiles(jsonObject["content"]);
+        }
+
+        public static async Task BuildContent(bool rebuildNow = false)
+        {
+            NPLLog.LogInfoHeadline(FontAwesome.Igloo, "BUILD CONTENT");
+
+            RuntimeBuilder.Rebuild = rebuildNow;
+            try
+            {
+                if (RuntimeBuilder.LaunchDebugger && !Debugger.IsAttached)
+                {
+                    try
+                    {
+                        Debugger.Launch();
+                        var currentProcess = Process.GetCurrentProcess();
+                        Log.Warning("Waiting for debugger to attach:");
+                        Log.Warning($"({currentProcess.MainModule.FileName} PID {currentProcess.Id}).");
+                        Log.Warning("Press the 'Cancel' button below to continue without debugger.");
+                        while (!Debugger.IsAttached)
+                        {
+                            if (CancelBuildContent)
+                            {
+                                break;
+                            }
+                            Thread.Sleep(100);
+                        }
+                    }
+                    catch (NotImplementedException e)
+                    {
+                        NPLLog.LogException(e, "The debugger is not implemented under Mono and thus is not supported on your platform.");
+                    }
+                }
+                if (!CancelBuildContent)
+                {
+                    GetAllContentFiles(out var filesToCopy, out var filesToBuild);
+
+                    RuntimeBuilder.RegisterCopyContent(filesToCopy.ToArray());
+                    RuntimeBuilder.RegisterBuildContent(filesToBuild.ToArray());
+
+                    await RuntimeBuilder.BuildContent();
+
+                    NPLLog.LogInfoHeadline(FontAwesome.Igloo, "BUILD FINISHED");
+                }
+                else NPLLog.LogInfoHeadline(FontAwesome.Igloo, "BUILD CANCELD");
+            }
+            catch (Exception e) { NPLLog.LogException(e, "BUILD FAILED"); }
+
+            CancelBuildContent = false;
+            BuildContentRunning = false;
+            RuntimeBuilder.Rebuild = false;
+        }
+
+        private static void ParseAllContentFiles(JsonNode jsonContent)
+        {
+            var content = jsonContent.AsObject().AsEnumerable();
+            for (int i = 0; i < content.Count(); i++)
+            {
+                var data = content.ToArray()[i];
+                var categoryObject = jsonContent[data.Key];
+
+                var importerName = data.Value["importer"].ToString();
+                var processorName = data.Value["processor"].ToString();
+
+                PipelineTypes.GetTypeDescriptions(Path.GetExtension(data.Value["path"].ToString()),
+                                out var outImporter, out var outProcessor);
+
+                var nplItem = new ContentItem(data.Key, importerName, processorName);
+                foreach (var categoryItem in categoryObject.AsObject())
+                {
+                    var itemKey = categoryItem.Key; //e.g. path
+                    var itemValue = categoryItem.Value; //e.g. "C:\\"
+
+                    nplItem.SetParameter(itemKey, itemValue);
+                }
+                ContentList.Add(data.Key, nplItem);
+            }
+        }
+
+        public static void GetAllContentFiles(out List<string> copyFiles, out List<string> buildFiles)
+        {
+            copyFiles = new List<string>();
+            buildFiles = new List<string>();
+
+            ContentRoot = ContentRoot.Replace("\\", "/");
+            if (ContentRoot.StartsWith("./"))
+            {
+                ContentRoot = ContentRoot.Substring(2, ContentRoot.Length - 2);
+            }
+
+            foreach (var item in ContentList)
+            {
+                string path = item.Value.Path.ToString().Replace("\\", "/");
+                if (path.Contains("../"))
+                {
+                    throw new Exception("'path' is not allowed to contain '../'! Use 'root' property to specify a different root instead.");
+                }
+
+                if (path.StartsWith('$'))
+                {
+                    // $ means that the path will not have the root appended to it.
+                    path = path.TrimStart('$');
+                }
+                else if (ContentRoot != "")
+                {
+                    // Appending root now so that we would work with full paths.
+                    path = Path.Combine(ContentRoot, path);
+                }
+
+                var fileName = Path.GetFileName(path);
+                var filePath = Path.GetDirectoryName(path);
+                if (string.IsNullOrEmpty(filePath))
+                {
+                    filePath = Directory.GetCurrentDirectory();
+                }
+
+                try
+                {
+                    var searchOpt = SearchOption.TopDirectoryOnly;
+                    if (item.Value.Recursive)
+                    {
+                        searchOpt = SearchOption.AllDirectories;
+                    }
+                    else searchOpt = SearchOption.TopDirectoryOnly;
+
+                    if (item.Value.Action == Enums.BuildAction.Copy)
+                    {
+                        copyFiles.AddRange(Directory.GetFiles(filePath, fileName, searchOpt));
+                    }
+                    else if (item.Value.Action == Enums.BuildAction.Build)
+                    {
+                        buildFiles.AddRange(Directory.GetFiles(filePath, fileName, searchOpt));
+                    }
+                }
+                catch (Exception e)
+                {
+                    NPLLog.LogException(e);
+                }
+            }
+        }
+
+        private static string[] GetAllReferences(JsonArray references)
+        {
+            if (PipelineTypes.IsDirty)
+            {
+                var combinedReferences = new List<string>();
+                foreach (var item in references)
+                {
+                    var reference = Environment.ExpandEnvironmentVariables(item.ToString());
+                    if (File.Exists(reference.ToString()))
+                    {
+                        combinedReferences.Add(Path.GetFullPath(reference.ToString()));
+                    }
+                    else if (Directory.Exists(Path.GetDirectoryName(reference.ToString())))
+                    {
+                        var dir = Path.GetDirectoryName(reference.ToString());
+                        var matchingFiles = Directory.GetFiles(dir, "*.dll", SearchOption.AllDirectories).ToList();
+                        foreach (var file in matchingFiles)
+                        {
+                            combinedReferences.Add(Path.GetFullPath(file));
+                        }
+                    }
+                    else
+                    {
+                        var matchingFiles = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.dll", SearchOption.AllDirectories).ToList();
+                        foreach (var file in matchingFiles)
+                        {
+                            combinedReferences.Add(Path.GetFullPath(file));
+                        }
+                    }
+                }
+                return combinedReferences.Distinct()
+                    .Where(x => !string.IsNullOrEmpty(x.ToString()))
+                    .ToArray();
+            }
+            return null;
+        }
+    }
+}
--- NPLEditor/Program.cs
+++ NPLEditor/Program.cs
@@ -1,45 +1,104 @@
-﻿using System.IO;
-using System.Runtime.InteropServices;
-using NPLEditor;
+﻿using NPLEditor;
 using NPLEditor.Data;
 using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
 
-// Ensure DPI-Awareness isn't lost for the dotnet tool.
-if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+public partial class Program
 {
-    [DllImport("user32.dll")]
-    static extern bool SetProcessDPIAware();
+    public static async Task Main(string[] args)
+    {
+        // Create the logs directory.
+        Directory.CreateDirectory(AppSettings.LogsPath);
 
-    SetProcessDPIAware();
+        // The general log file should always regenerate.
+        if (File.Exists(AppSettings.AllLogPath)) File.Delete(AppSettings.AllLogPath);
+
+        // Create the serilog logger.
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.File(AppSettings.AllLogPath,
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose,
+                rollOnFileSizeLimit: true)
+            .WriteTo.File(AppSettings.ImportantLogPath,
+                rollingInterval: RollingInterval.Day,
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning,
+                rollOnFileSizeLimit: true)
+            .WriteTo.Debug(
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Debug)
+            .WriteTo.Console(
+            restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Debug)
+            .WriteTo.NPLEditorSink(
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose)
+            .CreateLogger();
+
+        // Parse command-line arguments
+        var arguments = ParseArguments(args);
+        if (arguments.ContainsKey("build"))
+        {
+#if DEBUG
+            string projectDir = Directory.GetParent(AppSettings.LocalContentPath).Parent.Parent.FullName;
+            string contentDir = Path.Combine(projectDir, "Content");
+            Directory.SetCurrentDirectory(contentDir);
+#else
+            Directory.SetCurrentDirectory(Path.Combine(Directory.GetCurrentDirectory(), "Content"));
+#endif
+            var nplJsonFilePath = args[1];
+            try
+            {
+                string jsonString;
+                using (var fs = new FileStream(nplJsonFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (var reader = new StreamReader(fs))
+                {
+                    jsonString = reader.ReadToEnd();
+                }
+                var jsonObject = JsonNode.Parse(jsonString);
+
+                ContentBuilder.Init(jsonObject);
+
+                await ContentBuilder.BuildContent();
+            }
+            catch (Exception e) { NPLLog.LogException(e, "ERROR", true); }
+        }
+        else RunGame();
+    }
+
+    private static void RunGame()
+    {
+        // Log that main initialize begins.
+        NPLLog.LogInfoHeadline(FontAwesome.Flag, "INITIALIZE");
+
+        // Ensure DPI-Awareness isn't lost for the dotnet tool.
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            [DllImport("user32.dll")]
+            static extern bool SetProcessDPIAware();
+
+            SetProcessDPIAware();
+        }
+        using var game = new Main();
+        game.Run();
+    }
+
+    private static Dictionary<string, string> ParseArguments(string[] args)
+    {
+        var arguments = new Dictionary<string, string>();
+        foreach (var arg in args)
+        {
+            var splitArg = arg.Split('=');
+            if (splitArg.Length == 2)
+            {
+                arguments[splitArg[0]] = splitArg[1];
+            }
+            else
+            {
+                arguments[splitArg[0]] = null;
+            }
+        }
+        return arguments;
+    }
 }
-
-// Create the logs directory.
-Directory.CreateDirectory(AppSettings.LogsPath);
-
-// The general log file should always regenerate.
-if (File.Exists(AppSettings.AllLogPath)) File.Delete(AppSettings.AllLogPath);
-
-// Create the serilog logger.
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Verbose()
-    .WriteTo.File(AppSettings.AllLogPath,
-        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose,
-        rollOnFileSizeLimit: true)
-    .WriteTo.File(AppSettings.ImportantLogPath,
-        rollingInterval: RollingInterval.Day,
-        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning,
-        rollOnFileSizeLimit: true)
-    .WriteTo.Debug(
-        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Debug)
-    .WriteTo.Console(
-    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Debug)
-    .WriteTo.NPLEditorSink(
-        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose)
-    .CreateLogger();
-
-// Log that main initialize begins.
-NPLLog.LogInfoHeadline(FontAwesome.Flag, "INITIALIZE");
-
-// Main initialize.
-using var game = new Main();
-game.Run();
